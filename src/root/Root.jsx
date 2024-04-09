5269cfe4b6382e232331e5567daa071498727640
shakibchakladar <shakibchakladar@gmail.com>
2024-04-09 15:25:02 +0600

    navbar added

--- src/root/Root.jsx
+++ src/root/Root.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router";
+import Nav from "../components/Nav";
 
 const Root = () => {
     return (
         <div>
+            <Nav></Nav>
             <Outlet></Outlet>
             
         </div>
