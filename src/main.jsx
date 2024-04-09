d798faccef4c872cdd6198f6ed290403b08e436d
shakibchakladar <shakibchakladar@gmail.com>
2024-04-09 12:35:54 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./root/Root";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element:<Root></Root>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
